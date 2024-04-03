e2ce8737f93947670928de80b9a40b7bc72e07ca
Maxwel <maxwellbakora@gmail.com>
2024-04-03 12:36:32 +0300

    Finished on this assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+  }
+  
+  function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let farePrice = 0;
+    
+    if (distance <= 400) {
+      farePrice = 0;
+    } else if (distance > 400 && distance <= 2000) {
+      farePrice = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      farePrice = 25;
+    } else {
+      return 'cannot travel that far';
+    }
+    
+    return farePrice;
+  }
+  
